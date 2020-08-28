0df450ba059b56526d4bac0d44a0aef4fb66065d
Bujia <guobujia@gmail.com>
2020-08-29 01:57:48 +0300

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -11,12 +11,13 @@ Use different threads for each file and combine the result
 """
 import json
 import pandas as pd
-df = pd.read_json("data.json")
-#Flip the df
-df = df.transpose()
-df.index.name = "Fullname"
-#converting json to csv
-df.to_csv("data.csv")
+def part_one():
+    df = pd.read_json("data.json")
+    #Flip the df
+    df = df.transpose()
+    df.index.name = "Fullname"
+    #converting json to csv
+    df.to_csv("data.csv")
 
 
 def LastNameCounter(json):
@@ -41,13 +42,16 @@ def LastNameCounter(json):
                     data[surname][key][tmp] = 1 
     return data
 
-with open("data.json", "r") as json_file:
-    json_data = json.load(json_file)
+def main():
+    part_one()
+    with open("data.json", "r") as json_file:
+        json_data = json.load(json_file)  
+    
+    data = LastNameCounter(json_data)
+    
+    json_object = json.dumps(data, indent = 3)
+    with open("myjson.json", "w") as outfile:
+        outfile.write(json_object)
 
-
-data = LastNameCounter(json_data)
-
-json_object = json.dumps(data, indent = 3)
-with open("myjson.json", "w") as outfile:
-    outfile.write(json_object)
-
+if __name__ == "__main__":
+   main()
