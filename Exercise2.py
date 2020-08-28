6dc2cf97bdd0455ddd7b4e6d1a2462e6bf42193c
Bujia <guobujia@gmail.com>
2020-08-28 23:17:49 +0300

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -21,52 +21,24 @@ df.to_csv("data.csv")
 
 def LastNameCounter(json):
     data = {}  
-    for name in json:
+    for name, value in json.items():
         surname = name.split()[1]
-        flag = 0
         #Creating nested dict with lastname stats 
         if surname not in data:
-            data[surname] = {}
-            data[surname]["count"] = 1
-            flag = 1
-            
-            data[surname]["age"] = {}
-            age = json[name]["age"]
-            data[surname]["age"][str(age)] = 1
-            
-            data[surname]["address"] = {}
-            address = json[name]["address"]
-            data[surname]["address"][address] = 1
-            
-            data[surname]["occupation"] = {}
-            occupation = json[name]["occupation"]
-            data[surname]["occupation"][occupation] = 1
-            
-        if flag == 0:   
-            #Checking if there is age in dict if not adding new age    
-            if json[name]["age"] not in data[surname]["age"]:
-                age = json[name]["age"]
-                data[surname]["age"][str(age)] = 1   
-            else:
-                data[surname]["age"][str(age)] = 11
-
-           #Checking if there is address in dict if not adding new address        
-            if json[name]["address"] not in data[surname]["address"]:
-                address = json[name]["address"]
-                data[surname]["address"][address] = 1
-            else:
-                data[surname]["address"][address] = 11
-            #Checking if there is occupation in dict if not adding new occupation   
-            if json[name]["occupation"] not in data[surname]["occupation"]:
-                occupation = json[name]["occupation"]          
-                data[surname]["occupation"][occupation] = 1
-            else:
-                data[surname]["occupation"][occupation] = 11
-            #checking if lastname count have been added
-            if flag == 0:
-                data[surname]["count"] += 1 
-            
-    
+            data[surname] = {
+                    "count": 1,
+                    "age": {str(value["age"]): 1},
+                    "address": {value["address"]: 1},
+                    "occupation": {value["occupation"]: 1}
+                }
+        else:
+            data[surname]["count"] += 1
+            for key in ["age", "address", "occupation"]:
+                tmp = str(value[key])
+                if tmp in data[surname][key]:
+                    data[surname][key][tmp] += 1
+                else:
+                    data[surname][key][tmp] = 1 
     return data
 
 with open("data.json", "r") as json_file:
